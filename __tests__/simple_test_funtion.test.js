31c887c0d97520d697bee15049a4ab7e7e41a6e2
Jussi <jussi.heinila@tuni.fi>
2025-11-21 10:55:04 +0200

    Files for testing if testing workflow works

---
+++ __tests__/simple_test_funtion.test.js
@@ -0,0 +1,9 @@
+const add = require('../src/simple_test_fucntion');  // Import the add function
+
+describe('add function', () => {
+  test('should correctly add two numbers', () => {
+    expect(add(1, 2)).toBe(3);
+    expect(add(-1, 1)).toBe(0);
+    expect(add(0, 0)).toBe(0);
+  });
+});
