cf74f90d9c00365e2dc5d022f819607f55c3e946
Tuukka Tikkakoski <tuukkatikkakoski@gmail.com>
2025-12-04 14:12:45 +0200

    added test for toString and made some configurations

---
+++ unit_tests/tests/toString.test.js
@@ -0,0 +1,43 @@
+jest.mock("../src/isSymbol", () => jest.fn());
+import toString from "../src/toString";
+import isSymbol from "../src/isSymbol";
+
+const INFINITY = 1 / 0;
+
+test("converts to string", () => {
+    // Setting isSymbol off so it doesn't trigger the if-clause
+    isSymbol.mockReturnValue(false);
+
+    // Testing a simple string value
+    expect(toString("hello")).toBe("hello");
+
+    // Testing both cases for array values, normal array and an array with null value
+    expect(toString([1, 2, "hello", 5])).toBe("1,2,hello,5");
+    expect(toString([1, null, "hi", 32])).toBe("1,,hi,32");
+
+    // Testing with a symbol, setting the isSymbol mock function 
+    // return value to true so the if-clause will go through
+    isSymbol.mockReturnValue(true);
+    const symbol1 = Symbol("hello");
+    expect(toString(symbol1)).toBe("Symbol(hello)");
+
+    isSymbol.mockReturnValue(false);
+
+    // Testing with integer values, case 1 where the value is -0
+    // and case 2 where the integer is more generic value
+    // case 3 for a negative value
+    let result = -0;
+    expect(toString(result)).toBe("-0");
+    
+    result = 1209;
+    expect(toString(result)).toBe("1209");
+
+    result = -52;
+    expect(toString(result)).toBe("-52");
+
+    // Lastly testing with an object, which probably will 
+    // not be used in any real cases
+    const randomObject = {"hello": 1, "hi": 50};
+    expect(toString(randomObject)).toBe("[object Object]");
+});
+
