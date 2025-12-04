79f0a1aaa0ad401077ebba0424333d38d70a5d95
Jussi Heinila <heinila.jussi@gmail.com>
2025-12-04 18:32:11 +0200

    Creating personal branch

---
+++ test/add.test.js
@@ -0,0 +1,7 @@
+import add from '../materials_from_course/src/add';
+
+describe('add function', () => {
+  test('should add two numbers correctly', () => {
+    expect(add(1,2)).toBe(3);
+  });
+});
