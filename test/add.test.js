27c1491d1acd26716d1507d1440c659d28530339
Jussi Heinila <heinila.jussi@gmail.com>
2025-12-07 20:55:05 +0200

    Added use of mocks. Added tests for floats. Restructured for clarity.

--- test/add.test.js
+++ test/add.test.js
@@ -1,19 +1,97 @@
-import add from '../materials_from_course/src/add';
+const mockCreateMathOperation = jest.fn();
 
-describe('add function', () => {
-  test('should add two numbers correctly', () => {
+jest.mock('../materials_from_course/src/.internal/createMathOperation.js', () => ({
+  __esModule: true,
+  default: mockCreateMathOperation
+}));
+
+describe("should add two integers correctly", () => {
+  beforeAll( () => {
+    jest.resetModules();
+    mockCreateMathOperation.mockImplementation( () => (a,b) => a+b);
+    add = require('../materials_from_course/src/add').default;
+  });
+  test("should return 3 with input (1,2)", () => {
     expect(add(1,2)).toBe(3);
-    expect(add(-1,2)).toBe(1);
+  });
+  test("should return 0 with input (-1,1)", () => {
+    expect(add(-1,1)).toBe(0);
+  });
+  test("should return -3 with input (-1,-2)", () => {
     expect(add(-1,-2)).toBe(-3);
   });
-  test('should return handle undefined inputs', () => {
-    expect(add(undefined,undefined)).toBe(0);
-    expect(add(1,undefined)).toBe(1);
+  // Expected results need further consideration
+  test("should return MAX_VALUE with input (MAX_VALUE,1)", () => {
+    const x = Number.MAX_VALUE;
+    expect(add(x,1)).toEqual(x);
+  });
+  test("should return 0 with input (MAX_VALUE,-MAX_VALUE)", () => {
+    const x = Number.MAX_VALUE + 1;
+    expect(add(x,-x)).toEqual(0);
+  });
+});
+
+describe("should add two decimal numbers correctly", () => {
+  beforeAll( () => {
+    jest.resetModules();
+    mockCreateMathOperation.mockImplementation( () => (a,b) => a+b);
+    add = require('../materials_from_course/src/add').default;
+  });
+  test("should return 1 with input (.25,.75)", () => {
+    expect(add(.25,.75)).toBe(1);
+  });
+  test("should return 0 with input (-.25,.25)", () => {
+    expect(add(-.25,.25)).toBe(0);
+  });
+  // Expected results need further consideration
+  test("should return MIN_VALUE with input (MIN_VALUE,MIN_VALUE)", () => {
+    const x = Number.MIN_VALUE;
+    expect(add(x,x)).toBe(2*x);
+  });
+  test("should return 0 with input (MIN_VALUE,-MIN_VALUE)", () => {
+    const x = Number.MIN_VALUE;
+    expect(add(x,-x)).toBe(0);
+  });
+});
+
+describe("should handle undefined inputs", () => {
+  beforeAll( () => {
+    jest.resetModules();
+    mockCreateMathOperation.mockImplementation( () => (a,b) => {
+      return (a == null && b == null) ? 0 :
+             (a == null) ? b : a;
+    });
+    add = require('../materials_from_course/src/add').default;
+  });
+  test("should return 0 for (undefined,null)", () => {
+    expect(add(undefined,null)).toBe(0);
+  });
+  test("should return 1 for (undefined,1)", () => {
     expect(add(undefined,1)).toBe(1);
   });
-  test('should return NaN with wrong type of input', () => {
+  test("should return 1 for (1,null)", () => {
+    expect(add(1,null)).toBe(1);
+  });
+});
+
+describe("should return NaN with wrong type of input", () => {
+  beforeAll( () => {
+    jest.resetModules();
+    mockCreateMathOperation.mockImplementation( () => (a,b) => {
+      if (typeof a === 'string' && typeof b === 'string') {
+        return a + b;
+      }
+      return 0;
+    });
+    add = require('../materials_from_course/src/add').default;
+  });
+  test("should return NaN for (true,false)", () => {
+    expect(add(true,false)).toBe(NaN);
+  });
+  test("should return NaN for ('a','b')", () => {
+    expect(add('a','b')).toBe(NaN);
+  });
+  test("should return NaN for ('1','2')", () => {
     expect(add('1','2')).toBe(NaN);
-    expect(add('a',2)).toBe(NaN);
-    expect(add(1,'b')).toBe(NaN);
   });
 });
