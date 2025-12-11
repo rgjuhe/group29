d72b5d302ece65f0e37f7c3c0d6c5833b1ccd9e2
Tuukka Tikkakoski <tuukkatikkakoski@gmail.com>
2025-12-11 14:57:57 +0200

    Changed tests for toNumber to hava smaller test blocks instead of one big test block

--- tests/toNumber.test.js
+++ tests/toNumber.test.js
@@ -5,40 +5,122 @@ import isObject from "../materials_from_course/src/isObject";
 import isSymbol from "../materials_from_course/src/isSymbol";
 import toNumber from "../materials_from_course/src/toNumber";
 
-
-test("turns arguments into numerical values if possible", () => {
-    isObject.mockReturnValue(false);
-    isSymbol.mockReturnValue(false);
-
-    // Testing with integer and float values.
-    expect(toNumber(3)).toBe(3);
-    expect(toNumber(3.2)).toBe(3.2);
-    expect(toNumber(0)).toBe(0);
-    expect(toNumber(-23)).toBe(-23);
-
-    // Testing with symbol
-    isSymbol.mockReturnValue(true);
-    expect(toNumber(Symbol())).toBe(NaN);
-
-    // Testing with objects
-    isSymbol.mockReturnValue(false);
-    isObject.mockReturnValue(true);
-    expect(toNumber({'a': 3, 'b': 4})).toBe(NaN);
-    expect(toNumber([2, 3, 4])).toBe(NaN);
-    expect(toNumber([4])).toBe(4);
-    isObject.mockReturnValue(false);
-
-    // Testing with strings
-    expect(toNumber("3.4")).toBe(3.4);
-    expect(toNumber("hello")).toBe(NaN);
-
-    // Testing with infinity
-    expect(toNumber(Infinity)).toBe(Infinity);
-    expect(toNumber(-Infinity)).toBe(-Infinity);
-
-    // Testing with octal, binary and hexadecimal values
-    expect(toNumber(0o10)).toBe(8);
-    expect(toNumber(0b00000010)).toBe(2);
-    expect(toNumber(0xFF)).toBe(255);
+describe("should return numerical values as they are", () => {
     
+    test("should return integer as integer", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(3)).toBe(3);
+    });
+
+    test("should return float as float", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(3.2)).toBe(3.2);
+    });
+
+    test("should return 0 as 0", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(0)).toBe(0);
+    });
+
+    test("should return negative integer as negative integer", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(-23)).toBe(-23);
+    });
+
+});
+
+describe("should return symbol as NaN", () => {
+    
+    test("should return symbol as NaN", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(true);
+        expect(toNumber(Symbol())).toBe(NaN);
+    });
+
+});
+
+describe("should return correct values for objects", () => {
+    
+    test("should return object as NaN", () => {
+        isObject.mockReturnValue(true);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber({'a': 3, 'b': 4})).toBe(NaN);
+    });
+
+    test("should return array with multiple values as NaN", () => {
+        isObject.mockReturnValue(true);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber([2, 3, 4])).toBe(NaN);
+    });
+
+    test("should return array with one value as that value", () => {
+        isObject.mockReturnValue(true);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber([4])).toBe(4);
+    });
+
+});
+
+describe("should return correct values for strings", () => {
+    
+    test("should return string with numerical value as the value", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber("3.4")).toBe(3.4);
+    });
+
+    test("should return any other string as NaN", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber("hello")).toBe(NaN);
+    });
+
+    test("should return any other string as NaN", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber("[1]")).toBe(NaN);
+    });
+
+});
+
+describe("should return Infinity", () => {
+    
+    test("should return Infinity as Infinity", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(Infinity)).toBe(Infinity);
+    });
+
+    test("should return -Infinity as -Infinity", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(-Infinity)).toBe(-Infinity);
+    });
+
+});
+
+describe("should return correct values for binary, octal and hexadecimal numbers", () => {
+    
+    test("should return octal as integer", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(0o10)).toBe(8);
+    });
+
+    test("should return binary as integer", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(0b00000010)).toBe(2);
+    });
+
+    test("should return hexadecimal as integer", () => {
+        isObject.mockReturnValue(false);
+        isSymbol.mockReturnValue(false);
+        expect(toNumber(0xFF)).toBe(255);
+    });
+
 });
