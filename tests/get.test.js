89a03df9325c1dc206bfb2e7dab2b519eca38d52
Tuukka Tikkakoski <tuukkatikkakoski@gmail.com>
2025-12-11 13:50:38 +0200

    Changed tests for get function to have smaller test blocks instead of one big test block

--- tests/get.test.js
+++ tests/get.test.js
@@ -2,27 +2,63 @@ jest.mock("../materials_from_course/src/.internal/baseGet", () => jest.fn());
 import baseGet from "../materials_from_course/src/.internal/baseGet";
 import get from "../materials_from_course/src/get";
 
+const object = {a: [{b: {c: 34}}, {d: [2, 3, 4]}]};
 
-test("gets values from path of object", () => {
+describe("should handle undefined inputs", () => {
 
-    const object = {a: [{b: {c: 34}}, {d: [2, 3, 4]}]};
+    test("should return undefined for integer as first argument", () => {
+        baseGet.mockReturnValue(undefined);
+        expect(get(2, "a[0].b")).toBe(undefined);
+    });
 
-    // Setting the baseGet mock function to undefined
-    // It doesn't really matter here but the expected return value is undefined here.
-    baseGet.mockReturnValue(undefined);
+    test("should return undefined for poor path (e.a.d)", () => {
+        baseGet.mockReturnValue(undefined);
+        expect(get(object, "e.a.d")).toBe(undefined);
+    });
 
-    // Testing with different values that should always return undefined
-    expect(get(2, "a[0].b")).toBe(undefined);
-    expect(get(object, "e.a.d")).toBe(undefined);
-    expect(get(object, "a[3]")).toBe(undefined);
-    expect(get(object, "a[-1]")).toBe(undefined);
+    test("should return undefined for out of bounds path (a[3])", () => {
+        baseGet.mockReturnValue(undefined);
+        expect(get(object, "a[3]")).toBe(undefined);
+    });
 
-    // Setting the baseGet value to 34
-    baseGet.mockReturnValue(34);
+    test("should return undefined for negative index of path (a[-1])", () => {
+        baseGet.mockReturnValue(undefined);
+        expect(get(object, "a[-1]")).toBe(undefined);
+    });
 
-    // Testing if the baseGet value works.
-    // Trying with null is important because if it would not work, then the baseGet
-    // value would overwrite it.
-    expect(get(object, "a[0].b.c")).toBe(34);
-    expect(get(null, "a[0].b")).toBe(undefined);
+    test("should return undefined for null object", () => {
+        baseGet.mockReturnValue(undefined);
+        expect(get(null, "a[0]")).toBe(undefined);
+        expect(baseGet).not.toHaveBeenCalled();
+    });
 });
+
+describe("should handle default value", () => {
+
+    test("should return default value for null object", () => {
+        baseGet.mockReturnValue(undefined);
+        expect(get(null, "a[0]")).toBe(undefined);
+    });
+
+    test("should return undefined default value for poor path", () => {
+        baseGet.mockReturnValue(undefined);
+        expect(get(object, "e.a.d")).toBe(undefined);
+    });
+
+});
+
+describe("should return a value for correct object and path", () => {
+
+    test("should return 34 for ", () => {
+        baseGet.mockReturnValue(34);
+        expect(get(object, "a[0].b.c")).toBe(34);
+    });
+
+    test("should return undefined for null object when baseGet is not undefined", () => {
+        baseGet.mockReturnValue(34);
+        expect(get(null, "a[0].b")).toBe(undefined);
+        expect(baseGet).not.toHaveBeenCalled();
+    });
+
+});
+
