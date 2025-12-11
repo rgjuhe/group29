91a43a71f140c0d2ca378a75e7d376b7cfb600a1
Tuukka Tikkakoski <tuukkatikkakoski@gmail.com>
2025-12-11 16:16:54 +0200

    Changed tests for reduce function to have smaller test blocks that just one big test block. Also fixed the test not working correctly.

--- tests/reduce.test.js
+++ tests/reduce.test.js
@@ -3,24 +3,23 @@ jest.mock("../materials_from_course/src/.internal/baseEach.js", () => jest.fn())
 jest.mock("../materials_from_course/src/.internal/baseReduce.js", () => jest.fn());
 
 import arrayReduce from "../materials_from_course/src/.internal/arrayReduce";
-import baseEach from "../materials_from_course/src/.internal/baseEach";
 import baseReduce from "../materials_from_course/src/.internal/baseReduce";
 import reduce from "../materials_from_course/src/reduce";
 
-test("reduces collection to some accumulated value", () => {
+describe("should call the right functions for arrays and objects", () => {
 
-    const testArray = [2, 53, 5, 18, 22];
-    const testObject = {"a": 1, "b": 3, "c": 2, "d": 1, "e": 10, "f": 2, "g": 1};
+    test("should call arrayReduce when using an array", () =>  {
+        arrayReduce.mockReturnValue(9);
+        baseReduce.mockReturnValue({ '1': ['a', 'c'], '2': ['b'] });
+        expect(reduce([2, 3, 4], (sum, n) => sum + n, 0)).toBe(9);
+    });
 
-    arrayReduce.mockReturnValue(100);
-
-    expect(reduce(testArray, (sum, n) => sum + n, 0)).toBe(100);
-
-    baseReduce.mockReturnValue({"1": ["a", "d", "g"], "2": ["c", "f"], "3": ["b"], "10": ["e"]});
-
-    expect(reduce(testObject, (result, value, key) => 
-          (result[value] || (result[value] = [])).push(key), {}))
-          .toBe({"1": ["a", "d", "g"], "2": ["c", "f"], "3": ["b"], "10": ["e"]});
-
+    test("should call baseReduce when using an object", () => {
+        arrayReduce.mockReturnValue(9);
+        baseReduce.mockReturnValue({ '1': ['a', 'c'], '2': ['b'] });
+        expect(reduce({ 'a': 1, 'b': 2, 'c': 1 }, (result, value, key) => {
+            (result[value] || (result[value] = [])).push(key)}))
+            .toEqual({ '1': ['a', 'c'], '2': ['b'] });
+    });
 
 });
