bc0e6e0e6fe97f5ae6055823cdf7979860b0b01d
Tuukka Tikkakoski <tuukkatikkakoski@gmail.com>
2025-12-11 14:04:32 +0200

    Changed tests for toInteger function to be in smaller test blocks instead of one big test block

--- tests/toInteger.test.js
+++ tests/toInteger.test.js
@@ -4,22 +4,63 @@ import toFinite from "../materials_from_course/src/toFinite";
 import toInteger from "../materials_from_course/src/toInteger";
 
 
-test("turns strings, floats and integers into integers", () => {
-    toFinite.mockReturnValue(0);
+describe("should return 0 for non-convertable values", () => {
+    
+    test("should return 0 for symbol", () => {
+        toFinite.mockReturnValue(0);
+        expect(toInteger(Symbol())).toBe(0);
+    });
 
-    // Testing with some values that are not convertable to integers
-    expect(toInteger(Symbol())).toBe(0);
-    expect(toInteger({'a': 2, 'b': 31})).toBe(0);
+    test("should return 0 for object", () => {
+        toFinite.mockReturnValue(0);
+        expect(toInteger({'a': 2, 'b': 31})).toBe(0);
+    });
 
-    // Testing with floats and strings
-    toFinite.mockReturnValue(4.5);
-    expect(toInteger("4.5")).toBe(4);
-    expect(toInteger(4.5)).toBe(4);
+});
 
-    // Testing with infinity and -infinity
-    toFinite.mockReturnValue(1.7976931348623157e+308);
-    expect(toInteger(Infinity)).toBe(1.7976931348623157e+308);
-    toFinite.mockReturnValue(-1.7976931348623157e+308);
-    expect(toInteger(-Infinity)).toBe(-1.7976931348623157e+308);
+describe("should return integer for floats and integers", () => {
+    
+    test("should return 4 for 4", () => {
+        toFinite.mockReturnValue(4);
+        expect(toInteger(4)).toBe(4);
+    });
+
+    test("should return 4 for 4.5", () => {
+        toFinite.mockReturnValue(4.5);
+        expect(toInteger(4.5)).toBe(4);
+    });
+
+});
+
+describe("should return 0 or some value for strings", () => {
+    
+    test("should return 4 if string is 4.5", () => {
+        toFinite.mockReturnValue(4.5);
+        expect(toInteger("4.5")).toBe(4);
+    });
+
+    test("should return 0 if string is some non-numerical value", () => {
+        toFinite.mockReturnValue(0);
+        expect(toInteger("hello")).toBe(0);
+    });
+
+    test("should return 4 if string is 4", () => {
+        toFinite.mockReturnValue(4);
+        expect(toInteger("4")).toBe(4);
+    });
+
+});
+
+describe("should return min or max integer value for infinity and -infinity", () => {
+
+    test("should return 1.7976931348623157e+308 for Infinity", () => {
+        toFinite.mockReturnValue(1.7976931348623157e+308);
+        expect(toInteger(Infinity)).toBe(1.7976931348623157e+308);
+    });
+
+    test("should return -1.7976931348623157e+308 for -Infinity", () => {
+        toFinite.mockReturnValue(-1.7976931348623157e+308);
+        expect(toInteger(-Infinity)).toBe(-1.7976931348623157e+308);
+    });
 
 });
