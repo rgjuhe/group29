dbbfb24ec448bca50dd18c39cd8810935a7c3fc8
Jussi Heinila <heinila.jussi@gmail.com>
2025-12-12 13:43:47 +0200

    Added tests for every and keys.

---
+++ test/every.test.js
@@ -0,0 +1,45 @@
+import every from '../materials_from_course/src/every';
+
+describe("should return true when all items fulllfil predicate", () => {
+  test("should return true for [-10,4] and function ((item) => item < 5)", () => {
+    expect(every([-10,4], ((item) => item < 5) )).toBe(true);
+  });
+  test("should return true for [2,4,6] and function ((item) => item%2 == 0)", () => {
+    expect(every([2,4,6,], ((item) => item%2 == 0) )).toBe(true);
+  });
+});
+
+describe("should handle empty array correctly", () => {
+  test("should return true for empty array", () => {
+    expect(every([], Boolean)).toBe(true);
+  });
+});
+
+describe("should return false when some item doesn't items fulllfil predicate", () => {
+  test("should return false for [-10,5] and function ((item) => item < 5) ", () => {
+    expect(every([-10,5], ((item) => item < 5) )).toBe(false);
+  });
+  test("should return false for [2,4,5] and function ((item) => item%2 == 0) ", () => {
+    expect(every([2,4,5], ((item) => item%2 == 0) )).toBe(false);
+  });
+});
+
+
+describe("should return false when some item's type doesn't match predicate", () => {
+  test("should return false for [undefined,5] and function ((item) => item < 5) ", () => {
+    expect(every([undefined,5], ((item) => item < 5) )).toBe(false);
+    expect(every([2,4,5], ((item) => item.length ) )).toBe(false);
+  });
+  test("should return false for [2,'abc] and function ((item) => item.length) ", () => {
+    expect(every([2,'abc',], ((item) => item.length ) )).toBe(false);
+  });
+});
+
+describe("should handle undefined input correctly", () => {
+  test("should return throw TypeError for undefined array", () => {
+    expect( () => {every(undefined, Boolean);}).toThrow(TypeError);
+  });
+  test("should throw TypeError for undefined predicate", () => {
+    expect( () => {every([1,2], undefined);}).toThrow(TypeError);
+  });
+});
---
+++ test/keys.test.js
@@ -0,0 +1,79 @@
+//import keys from '../materials_from_course/src/keys';
+import arrayLikeKeys from '../materials_from_course/src/.internal/arrayLikeKeys.js';
+import isArrayLike from '../materials_from_course/src/isArrayLike.js';
+
+const mockArrayLikeKeys = jest.fn();
+jest.mock('../materials_from_course/src/.internal/arrayLikeKeys.js', () => ({
+  __esModule: true,
+  default: mockArrayLikeKeys
+}));
+const mockIsArrayLike = jest.fn();
+jest.mock('../materials_from_course/src/isArrayLike.js', () => ({
+  __esModule: true,
+  default: mockIsArrayLike
+}));
+
+describe("should return correct keys for arrays", () => {
+  let keys;
+  beforeAll( () => {
+    jest.resetModules();
+    mockArrayLikeKeys.mockImplementation( (value, inherited) => {
+      return value.map((_, i) => String(i));
+    });
+    mockIsArrayLike.mockImplementation( (value) => {
+      return true;
+    });
+    keys = require('../materials_from_course/src/keys').default;
+  });
+  test("should return ['0','1','2'] for ['a','b','c']", () => {
+    const arr = ['a','b','c'];
+    
+    expect(keys(arr)).toEqual(["0","1","2"]);
+  });
+  test("should handle large arrays", () => {
+    const size = 100000;
+    const arr = Array.from({length: size}, (x) => x);
+    const expRes = Array.from({length: size}, (_,i) => String(i));
+    expect(keys(arr)).toEqual(expRes);
+  });
+});
+
+describe("should return correct keys for other arrayLike inputs", () => {
+  let keys;
+  beforeAll( () => {
+    jest.resetModules();
+    mockArrayLikeKeys.mockImplementation( (value, inherited) => {
+      return Array.from(value, (_, i) => String(i));
+    });
+    mockIsArrayLike.mockImplementation( (value) => {
+      return true;
+    });
+    keys = require('../materials_from_course/src/keys').default;
+  });
+  test("should return ['0','1','2'] for string 'hey'", () => {
+    const str = 'hey';
+    expect(keys(str)).toEqual(["0","1","2"]);
+  });
+  test("should return ['0','1','2','3','4'] for Uint8Array '[0,0,0,0,0]'", () => {
+    const uint8A = new Uint8Array(5);
+    expect(keys(uint8A)).toEqual(["0","1","2","3","4"]);
+  }); 
+});
+
+describe("Should return correct keys for simple objects", () => {
+  let keys;
+  beforeAll( () => {
+    jest.resetModules();
+    mockArrayLikeKeys.mockImplementation( () => {
+      return;
+    });
+    mockIsArrayLike.mockImplementation( (value) => {
+      return false;
+    });
+    keys = require('../materials_from_course/src/keys').default;
+  });
+  test("", () => {
+    const obj = {'a': 2, 'b':1, 'c':1};
+    expect(keys(obj)).toEqual(['a','b','c']);
+  })
+});
