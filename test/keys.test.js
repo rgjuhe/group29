64080962816f83c422133c4606af4163e7066d4f
Jussi Heinila <heinila.jussi@gmail.com>
2025-12-12 13:34:58 +0200

    Added better mock for isArrayLike. Added test for srings and Uint8Array.

--- test/keys.test.js
+++ test/keys.test.js
@@ -7,7 +7,11 @@ jest.mock('../materials_from_course/src/.internal/arrayLikeKeys.js', () => ({
   __esModule: true,
   default: mockArrayLikeKeys
 }));
-jest.mock('../materials_from_course/src/isArrayLike.js', () => jest.fn());
+const mockIsArrayLike = jest.fn();
+jest.mock('../materials_from_course/src/isArrayLike.js', () => ({
+  __esModule: true,
+  default: mockIsArrayLike
+}));
 
 describe("should return correct keys for arrays", () => {
   let keys;
@@ -16,11 +20,13 @@ describe("should return correct keys for arrays", () => {
     mockArrayLikeKeys.mockImplementation( (value, inherited) => {
       return value.map((_, i) => String(i));
     });
+    mockIsArrayLike.mockImplementation( (value) => {
+      return true;
+    });
     keys = require('../materials_from_course/src/keys').default;
   });
-  test("should return [0,1,2] for ['a','b','c']", () => {
+  test("should return ['0','1','2'] for ['a','b','c']", () => {
     const arr = ['a','b','c'];
-    isArrayLike.mockReturnValue(true);
     
     expect(keys(arr)).toEqual(["0","1","2"]);
   });
@@ -28,16 +34,45 @@ describe("should return correct keys for arrays", () => {
     const size = 100000;
     const arr = Array.from({length: size}, (x) => x);
     const expRes = Array.from({length: size}, (_,i) => String(i));
-    isArrayLike.mockReturnValue(true);
     expect(keys(arr)).toEqual(expRes);
   });
 });
 
+describe("should return correct keys for other arrayLike inputs", () => {
+  let keys;
+  beforeAll( () => {
+    jest.resetModules();
+    mockArrayLikeKeys.mockImplementation( (value, inherited) => {
+      return Array.from(value, (_, i) => String(i));
+    });
+    mockIsArrayLike.mockImplementation( (value) => {
+      return true;
+    });
+    keys = require('../materials_from_course/src/keys').default;
+  });
+  test("should return ['0','1','2'] for string 'hey'", () => {
+    const str = 'hey';
+    expect(keys(str)).toEqual(["0","1","2"]);
+  });
+  test("should return ['0','1','2','3','4'] for Uint8Array '[0,0,0,0,0]'", () => {
+    const uint8A = new Uint8Array(5);
+    expect(keys(uint8A)).toEqual(["0","1","2","3","4"]);
+  }); 
+});
+
 describe("Should return correct keys for simple objects", () => {
   let keys;
-  keys = require('../materials_from_course/src/keys').default;
+  beforeAll( () => {
+    jest.resetModules();
+    mockArrayLikeKeys.mockImplementation( () => {
+      return;
+    });
+    mockIsArrayLike.mockImplementation( (value) => {
+      return false;
+    });
+    keys = require('../materials_from_course/src/keys').default;
+  });
   test("", () => {
-    isArrayLike.mockReturnValue(false);
     const obj = {'a': 2, 'b':1, 'c':1};
     expect(keys(obj)).toEqual(['a','b','c']);
   })
